18189aec64e86474a54db8098f8368964cccd67a
kyyee <kyyeeyoung@163.com>
2018-04-12 21:11:14 +0800

    1.添加restful api版本控制 2.添加aop日志切面 3.添加异步方法支持 4.添加定时器任务支持

---
+++ src/main/java/com/company/springbootrestfulapiprojectseed/v1/config/SchedulerConfig.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2018.  kyyee All rights reserved.
+ */
+
+package com.company.springbootrestfulapiprojectseed.v1.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+
+/**
+ * @author kyyee
+ * 计划任务配置
+ */
+@Configuration
+@EnableScheduling
+public class SchedulerConfig implements SchedulingConfigurer {
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar scheduledTaskRegistrar) {
+
+    }
+}
---
+++ src/main/java/com/company/springbootrestfulapiprojectseed/v1/domain/EmployeeDO.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright (c) 2018.  kyyee All rights reserved.
+ */
+
+package com.company.springbootrestfulapiprojectseed.v1.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ * @author kyyee
+ */
+@Entity
+public class EmployeeDO {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    /**
+     * 创建时间（非空）
+     */
+    private Long gmtCreate;
+
+    /**
+     * 修改时间（可空）
+     */
+    private Long gmtModified;
+
+    private String name;
+
+    private Integer age;
+
+    private String department;
+
+    public EmployeeDO() {
+    }
+
+    public EmployeeDO(Long gmtCreate, Long gmtModified, String name, Integer age, String department) {
+        this.gmtCreate = gmtCreate;
+        this.gmtModified = gmtModified;
+        this.name = name;
+        this.age = age;
+        this.department = department;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getGmtCreate() {
+        return gmtCreate;
+    }
+
+    public void setGmtCreate(Long gmtCreate) {
+        this.gmtCreate = gmtCreate;
+    }
+
+    public Long getGmtModified() {
+        return gmtModified;
+    }
+
+    public void setGmtModified(Long gmtModified) {
+        this.gmtModified = gmtModified;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    @Override
+    public String toString() {
+        return "EmployeeDO{" +
+                "id=" + id +
+                ", gmtCreate=" + gmtCreate +
+                ", gmtModified=" + gmtModified +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", department='" + department + '\'' +
+                '}';
+    }
+}
