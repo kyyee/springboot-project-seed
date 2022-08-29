2646d56e6474be4b2c18ff0c37aa725b169dce3b
kyyee <kyyeeyoung@163.com>
2022-08-29 19:35:37 +0800

    feat(restTemplate配置): 代码合并

---
+++ src/main/java/com/kyyee/sps/config/WebSecurityConfig.java
@@ -0,0 +1,33 @@
+///*
+// * Copyright (c) 2018.  kyyee All rights reserved.
+// */
+//
+//package com.kyyee.sps.config;
+//
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.builders.WebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+///**
+// * @author kyyee
+// * 认证授权配置
+// */
+//@EnableWebSecurity
+//public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        super.configure(auth);
+//    }
+//
+//    @Override
+//    public void configure(WebSecurity web) throws Exception {
+//        super.configure(web);
+//    }
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        super.configure(http);
+//    }
+//}
