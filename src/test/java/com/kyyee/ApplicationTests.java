e73bbde14c9a68711ab98764a6c35d42320f892e
kyyee <kyyeeyoung@163.com>
2022-09-01 10:06:38 +0800

    feat(junit): 修改junit启动类错误

--- src/test/java/com/kyyee/ApplicationTests.java
+++ src/test/java/com/kyyee/ApplicationTests.java
@@ -4,29 +4,12 @@
 
 package com.kyyee;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.context.WebApplicationContext;
 
-import javax.annotation.Resource;
-
-@RunWith(SpringRunner.class)
 @SpringBootTest
-public class ApplicationTests {
-
-    private MockMvc mockMvc;
-
-    @Resource
-    private
-    WebApplicationContext wac;
-
-    String expectedJson;
-
+class ApplicationTests {
     @Test
-    public void contextLoads() {
+    void contextLoads() {
     }
-
 }
