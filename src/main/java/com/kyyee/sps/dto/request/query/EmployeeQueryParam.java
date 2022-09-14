92fc6aba8e25fb0b3fb6407d89fe0238ef2c742d
kyyee <kyyeeyoung@163.com>
2022-09-14 11:27:28 +0800

    feat(crud): 完善crud示例

--- src/main/java/com/kyyee/sps/dto/request/query/EmployeeQueryParam.java
+++ src/main/java/com/kyyee/sps/dto/request/query/EmployeeQueryParam.java
@@ -11,6 +11,9 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 /**
  * @author kyyee
  */
@@ -22,4 +25,16 @@ public class EmployeeQueryParam extends PageQuery {
 
     @Schema(description = "搜索关键字")
     private String keyword;
+
+    @Schema(description = "年龄")
+    private Integer age;
+
+    @Schema(description = "设备id，多个用英文逗号隔开", name = "org_ids", example = "1076231009206272,1076231009206273")
+    private List<Long> orgIds;
+
+    @Schema(description = "开始时间，毫秒时间戳", name = "begin_time")
+    private LocalDateTime beginTime;
+
+    @Schema(description = "结束时间，毫秒时间戳", name = "end_time")
+    private LocalDateTime endTime;
 }
--- src/main/java/com/kyyee/sps/mapper/primary/EmployeeMapper.java
+++ src/main/java/com/kyyee/sps/mapper/primary/EmployeeMapper.java
@@ -4,8 +4,11 @@
 
 package com.kyyee.sps.mapper.primary;
 
+import com.kyyee.sps.dto.request.query.EmployeeQueryParam;
 import com.kyyee.sps.mapper.BaseMapper;
 import com.kyyee.sps.model.primary.Employee;
+
+import java.util.List;
 
 /**
  * @author kyyee
@@ -13,4 +16,5 @@ import com.kyyee.sps.model.primary.Employee;
 @org.apache.ibatis.annotations.Mapper
 public interface EmployeeMapper extends BaseMapper<Employee, Long> {
 
+    List<Employee> selectByParam(EmployeeQueryParam param);
 }
